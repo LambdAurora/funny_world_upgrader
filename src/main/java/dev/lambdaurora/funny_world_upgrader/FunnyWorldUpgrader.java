91a894bbc1b81f281cb36564099262903d92642f
LambdAurora <aurora42lambda@gmail.com>
2021-04-11 21:23:29 +0200

    Actually fix heightmaps.

--- src/main/java/dev/lambdaurora/funny_world_upgrader/FunnyWorldUpgrader.java
+++ src/main/java/dev/lambdaurora/funny_world_upgrader/FunnyWorldUpgrader.java
@@ -17,13 +17,24 @@
 
 package dev.lambdaurora.funny_world_upgrader;
 
+import dev.lambdaurora.funny_world_upgrader.mixin.BiomeArrayAccessor;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Blocks;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
-import net.minecraft.util.Identifier;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.collection.PackedIntegerArray;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.ChunkSerializer;
 import net.minecraft.world.HeightLimitView;
+import net.minecraft.world.biome.source.BiomeArray;
+import net.minecraft.world.biome.source.BiomeSource;
+import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.dimension.DimensionType;
 
 /**
  * Represents the Funny World Upgrader mod.
@@ -69,5 +80,121 @@ public class FunnyWorldUpgrader implements ModInitializer {
             for (int i = 0; i < 4; i++)
                 list.add(new NbtList());
         }
+    }
+
+    public static boolean tryConvert(ServerWorld world,
+                                     ChunkPos pos, NbtCompound nbt,
+                                     BiomeSource biomeSource,
+                                     NbtCompound levelNbt, BiomeArray biomeArray,
+                                     ChunkSection[] sections) {
+        int dataVersion = FunnyWorldUpgrader.DATA_VERSION_THREAD_LOCAL.get();
+
+        if (dataVersion > 2692
+                || world.getDimension() != world.getRegistryManager().get(Registry.DIMENSION_TYPE_KEY).get(DimensionType.OVERWORLD_ID))
+            // New Chunk with proper format or non-overworld.
+            return true;
+
+        // BiomeArray upgrade (warning: this is bad)
+        var oldBiomeArray = new BiomeArray(world.getRegistryManager().get(Registry.BIOME_KEY),
+                OldOverworldHeightLimitView.SELF, pos, biomeSource,
+                levelNbt.contains("Biomes", 11) ? levelNbt.getIntArray("Biomes") : null);
+        for (int x = 0; x < 4; x++) {
+            for (int z = 0; z < 4; z++) {
+                for (int y = 0; y < 96; y++) {
+                    int oldY = y;
+                    if (y < 16) {
+                        oldY = 0;
+                    } else if (y >= 80) {
+                        oldY = 63;
+                    }
+                    var old = oldBiomeArray.getBiomeForNoiseGen(x * 4, oldY * 4, z * 4);
+                    var i = y << HORIZONTAL_SECTION_COUNT * 2 | z << HORIZONTAL_SECTION_COUNT | x;
+                    ((BiomeArrayAccessor) biomeArray).getData()[i] = old;
+                }
+            }
+        }
+
+        if (!DROP_PROTO_CHUNKS || ChunkSerializer.getChunkType(nbt) == ChunkStatus.ChunkType.LEVELCHUNK) {
+            for (int sectionIndex = 0; sectionIndex < sections.length; sectionIndex++) {
+                int sectionY = world.sectionIndexToCoord(sectionIndex);
+                if (sections[sectionIndex] == null) {
+                    // Custom logic needed!
+                    if (sectionY < 0) {
+                        // Time to put funny blocks?
+                        var section = new ChunkSection(sectionY);
+                        for (int x = 0; x < 16; x++) {
+                            for (int z = 0; z < 16; z++) {
+                                for (int y = 0; y < 16; y++) {
+                                    section.getContainer().set(x, y, z, Blocks.DEEPSLATE.getDefaultState());
+                                }
+                            }
+                        }
+
+                        if (sectionY == world.getBottomSectionCoord()) {
+                            // Copy bedrock.
+                            var oldBottomSection = sections[world.sectionCoordToIndex(0)];
+                            if (oldBottomSection != null) {
+                                for (int x = 0; x < 16; x++) {
+                                    for (int z = 0; z < 16; z++) {
+                                        for (int y = 0; y < 5; y++) {
+                                            if (oldBottomSection.getBlockState(x, y, z).isOf(Blocks.BEDROCK)) {
+                                                section.getContainer().set(x, y, z, Blocks.BEDROCK.getDefaultState());
+                                            }
+                                        }
+                                    }
+                                }
+                            } else {
+                                for (int x = 0; x < 16; x++) {
+                                    for (int z = 0; z < 16; z++) {
+                                        section.getContainer().set(x, 0, z, Blocks.BEDROCK.getDefaultState());
+                                    }
+                                }
+                            }
+                        }
+
+                        section.calculateCounts();
+                        sections[sectionIndex] = section;
+                    }
+                } else if (sectionY == 0) {
+                    var oldBottomSection = sections[sectionIndex];
+                    for (int x = 0; x < 16; x++) {
+                        for (int z = 0; z < 16; z++) {
+                            for (int y = 0; y < 5; y++) {
+                                if (oldBottomSection.getBlockState(x, y, z).isOf(Blocks.BEDROCK)) {
+                                    oldBottomSection.setBlockState(x, y, z, Blocks.DEEPSLATE.getDefaultState(), false);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (levelNbt.contains("Heightmaps", NbtElement.COMPOUND_TYPE)) {
+            var heightmaps = levelNbt.getCompound("Heightmaps");
+
+            heightmaps.getKeys().forEach(key -> {
+                var heightmapData = heightmaps.getLongArray(key);
+                var oldElementBits = MathHelper.log2DeBruijn(257);
+                var oldArray = new PackedIntegerArray(oldElementBits, 256);
+                System.arraycopy(heightmapData, 0, oldArray.getStorage(), 0, heightmapData.length);
+                var newElementBits = MathHelper.log2DeBruijn(world.getHeight() + 1);
+                var newArray = new PackedIntegerArray(newElementBits, 256);
+                for (int i = 0; i < 256; i++) {
+                    newArray.set(i, oldArray.get(i) - world.getBottomY());
+                }
+                heightmaps.putLongArray(key, newArray.getStorage());
+            });
+        }
+
+        tryFixNbtSections(levelNbt, "PostProcessing", world);
+
+        if (!DROP_PROTO_CHUNKS) {
+            tryFixNbtSections(levelNbt, "Lights", world);
+            tryFixNbtSections(levelNbt, "LiquidsToBeTicked", world);
+            tryFixNbtSections(levelNbt, "ToBeTicked", world);
+        }
+
+        return !(DROP_PROTO_CHUNKS && ChunkSerializer.getChunkType(nbt) == ChunkStatus.ChunkType.PROTOCHUNK);
     }
 }
--- src/main/java/dev/lambdaurora/funny_world_upgrader/mixin/ChunkSerializerMixin.java
+++ src/main/java/dev/lambdaurora/funny_world_upgrader/mixin/ChunkSerializerMixin.java
@@ -18,26 +18,22 @@
 package dev.lambdaurora.funny_world_upgrader.mixin;
 
 import dev.lambdaurora.funny_world_upgrader.FunnyWorldUpgrader;
-import dev.lambdaurora.funny_world_upgrader.OldOverworldHeightLimitView;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructureManager;
-import net.minecraft.util.collection.PackedIntegerArray;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.ChunkSerializer;
 import net.minecraft.world.ChunkTickScheduler;
 import net.minecraft.world.biome.source.BiomeArray;
 import net.minecraft.world.biome.source.BiomeSource;
-import net.minecraft.world.chunk.*;
+import net.minecraft.world.chunk.ChunkManager;
+import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.chunk.ProtoChunk;
+import net.minecraft.world.chunk.UpgradeData;
 import net.minecraft.world.chunk.light.LightingProvider;
-import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.poi.PointOfInterestStorage;
 import org.spongepowered.asm.mixin.Mixin;
@@ -45,8 +41,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import static dev.lambdaurora.funny_world_upgrader.FunnyWorldUpgrader.*;
 
 @Mixin(ChunkSerializer.class)
 public abstract class ChunkSerializerMixin {
@@ -63,116 +57,7 @@ public abstract class ChunkSerializerMixin {
                                      ChunkTickScheduler<Block> chunkBlockTickScheduler, ChunkTickScheduler<Fluid> chunkFluidTickScheduler,
                                      boolean lightOn, NbtList sectionsNbt, int verticalSectionsCount, ChunkSection[] sections,
                                      boolean hasSkyLight, ChunkManager chunkManager, LightingProvider lightingProvider) {
-        int dataVersion = FunnyWorldUpgrader.DATA_VERSION_THREAD_LOCAL.get();
-
-        if (dataVersion > 2692
-                || world.getDimension() != world.getRegistryManager().get(Registry.DIMENSION_TYPE_KEY).get(DimensionType.OVERWORLD_ID))
-            // New Chunk with proper format or non-overworld.
-            return;
-
-        // BiomeArray upgrade (warning: this is bad)
-        var oldBiomeArray = new BiomeArray(world.getRegistryManager().get(Registry.BIOME_KEY),
-                OldOverworldHeightLimitView.SELF, pos, biomeSource,
-                levelNbt.contains("Biomes", 11) ? levelNbt.getIntArray("Biomes") : null);
-        for (int x = 0; x < 4; x++) {
-            for (int z = 0; z < 4; z++) {
-                for (int y = 0; y < 96; y++) {
-                    int oldY = y;
-                    if (y < 16) {
-                        oldY = 0;
-                    } else if (y >= 80) {
-                        oldY = 63;
-                    }
-                    var old = oldBiomeArray.getBiomeForNoiseGen(x * 4, oldY * 4, z * 4);
-                    var i = y << HORIZONTAL_SECTION_COUNT * 2 | z << HORIZONTAL_SECTION_COUNT | x;
-                    ((BiomeArrayAccessor) biomeArray).getData()[i] = old;
-                }
-            }
-        }
-
-        if (!DROP_PROTO_CHUNKS || ChunkSerializer.getChunkType(nbt) == ChunkStatus.ChunkType.LEVELCHUNK) {
-            for (int sectionIndex = 0; sectionIndex < sections.length; sectionIndex++) {
-                int sectionY = world.sectionIndexToCoord(sectionIndex);
-                if (sections[sectionIndex] == null) {
-                    // Custom logic needed!
-                    if (sectionY < 0) {
-                        // Time to put funny blocks?
-                        var section = new ChunkSection(sectionY);
-                        for (int x = 0; x < 16; x++) {
-                            for (int z = 0; z < 16; z++) {
-                                for (int y = 0; y < 16; y++) {
-                                    section.getContainer().set(x, y, z, Blocks.DEEPSLATE.getDefaultState());
-                                }
-                            }
-                        }
-
-                        if (sectionY == world.getBottomSectionCoord()) {
-                            // Copy bedrock.
-                            var oldBottomSection = sections[world.sectionCoordToIndex(0)];
-                            if (oldBottomSection != null) {
-                                for (int x = 0; x < 16; x++) {
-                                    for (int z = 0; z < 16; z++) {
-                                        for (int y = 0; y < 5; y++) {
-                                            if (oldBottomSection.getBlockState(x, y, z).isOf(Blocks.BEDROCK)) {
-                                                section.getContainer().set(x, y, z, Blocks.BEDROCK.getDefaultState());
-                                            }
-                                        }
-                                    }
-                                }
-                            } else {
-                                for (int x = 0; x < 16; x++) {
-                                    for (int z = 0; z < 16; z++) {
-                                        section.getContainer().set(x, 0, z, Blocks.BEDROCK.getDefaultState());
-                                    }
-                                }
-                            }
-                        }
-
-                        section.calculateCounts();
-                        sections[sectionIndex] = section;
-                    }
-                } else if (sectionY == 0) {
-                    var oldBottomSection = sections[sectionIndex];
-                    for (int x = 0; x < 16; x++) {
-                        for (int z = 0; z < 16; z++) {
-                            for (int y = 0; y < 5; y++) {
-                                if (oldBottomSection.getBlockState(x, y, z).isOf(Blocks.BEDROCK)) {
-                                    oldBottomSection.setBlockState(x, y, z, Blocks.DEEPSLATE.getDefaultState(), false);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        if (levelNbt.contains("Heightmaps", NbtElement.COMPOUND_TYPE)) {
-            var heightmaps = levelNbt.getCompound("Heightmaps");
-
-            heightmaps.getKeys().forEach(key -> {
-                var heightmapData = heightmaps.getLongArray(key);
-                var oldElementBits = MathHelper.log2DeBruijn(256);
-                var oldArray = new PackedIntegerArray(oldElementBits, 256);
-                System.arraycopy(heightmapData, 0, oldArray.getStorage(), 0, heightmapData.length);
-                var newElementBits = MathHelper.log2DeBruijn(world.getHeight() + 1);
-                var newArray = new PackedIntegerArray(newElementBits, 256);
-                for (int i = 0; i < 256; i++) {
-                    newArray.set(i, oldArray.get(i) - world.getBottomY());
-                }
-                heightmaps.putLongArray(key, newArray.getStorage());
-            });
-        }
-
-        tryFixNbtSections(levelNbt, "PostProcessing", world);
-
-        if (!DROP_PROTO_CHUNKS) {
-            tryFixNbtSections(levelNbt, "Lights", world);
-            tryFixNbtSections(levelNbt, "LiquidsToBeTicked", world);
-            tryFixNbtSections(levelNbt, "ToBeTicked", world);
-        }
-
-        if (DROP_PROTO_CHUNKS && ChunkSerializer.getChunkType(nbt) == ChunkStatus.ChunkType.PROTOCHUNK) {
+        if (!FunnyWorldUpgrader.tryConvert(world, pos, nbt, biomeSource, levelNbt, biomeArray, sections))
             cir.setReturnValue(new ProtoChunk(pos, UpgradeData.NO_UPGRADE_DATA, world));
-        }
     }
 }
